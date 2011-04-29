d05d751cea4afeaf0f838fd0fe06bc5ed0063a5f
Roger Heim <roger.heim@gmail.com>
2011-04-28 23:52:10 -0400

    First test commit

---
+++ TestAndroidProject/src/com/aremaitch/testandroidongit/StartActivity.java
@@ -0,0 +1,18 @@
+package com.aremaitch.testandroidongit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: roger
+ * Date: 4/28/11
+ * Time: 11:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StartActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_activity);
+    }
+}
